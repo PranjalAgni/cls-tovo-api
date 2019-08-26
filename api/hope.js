f24a70cc4fa2d766bf5d84e73c2165065f26f8b4
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-08-27 04:13:57 +0530

    Added constants

--- api/hope.js
+++ api/hope.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const middleware = require('./middleware');
+const express = require("express");
+const cors = require("cors");
+const middleware = require("./middleware");
+// For Constants
+const constants = require("./utils/constants");
+
 const app = express();
 
 app.use(cors());
 
-app.post('/cms/tovo/v1/:route', middleware.understandRequest, (req, res) => {
-  const status = res.isRoute ? 200 : 404;
-  const data = res.data;
-  res
-    .json({
-      isRoute: res.isRoute,
-      ...data
-    })
-    .status(status);
-});
+app.post(
+    `${constants.rrdmsUrlPath}:route`,
+    middleware.understandRequest,
+    (req, res) => {
+        const status = res.isRoute ? 200 : 404;
+        const data = res.data;
+        res.json({
+            isRoute: res.isRoute,
+            ...data
+        }).status(status);
+    }
+);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || constants.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
