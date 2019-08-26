772468141b08ca4657d572556e98e873f879b816
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-08-26 22:39:04 +0530

    :tada: Minor Changes

--- api/middleware.js
+++ api/middleware.js
@@ -1,9 +1,7 @@
-const db = require("./db.json");
-const routeJSON = require("./routes.json");
+const db = require('./db.json');
+const routeJSON = require('./routes.json');
 const understandRequest = (req, res, next) => {
   const requiredRoute = req.params.route;
-  // const allRoutes = Object.keys(db);
-
   // Object Keys
   // filter if query is present
 
@@ -13,7 +11,7 @@ const understandRequest = (req, res, next) => {
   //   }
   // });
   const dbRoute = routeJSON[requiredRoute];
-  let result = "";
+  let result = '';
   if (dbRoute !== undefined) {
     result = dbRoute.substring(1);
   }
@@ -27,29 +25,25 @@ const understandRequest = (req, res, next) => {
   // const query = req.params.query;
   // const dataHolder = db[requiredRoute];
   const dataHolder = db[result];
-  console.log(dataHolder);
+  // console.log(dataHolder);
 
   res.isRoute = true;
+  console.log(req.query);
   if (Object.keys(req.query).length === 0) {
     // All the values of that key
     res.data = dataHolder;
   } else {
-    // Check if it array
-    if (dataHolder.length) {
-      const query = req.query.customerId;
-      // Filter values on the above query
-      // Assuming query is id
-      const dataResult = dataHolder.find(data => {
-        if (data.id == query) {
-          return data;
-        }
-      });
-      res.data = dataResult;
-    }
-    // TODO: On specific id return data
-    else {
-      res.data = dataHolder;
-    }
+    const query = req.query.customerId;
+    // Filter values on the above query
+    // Assuming query is id
+    const dataResult = dataHolder.find(data => {
+      if (data.id == query) {
+        return data;
+      }
+    });
+
+    //console.log(dataResult);
+    res.data = dataResult;
   }
 
   next();
--- api/server.js
+++
@@ -1,8 +0,0 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-
-server.use(router);
-server.listen(4040, () => {
-  console.log('JSON Server is running');
-});
