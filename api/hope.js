11c4845aeb713c9614601f87b63b07b1b7f8e383
Soham Das <sohamdas1587@gmail.com>
2019-08-26 22:10:42 +0530

    Fixed routing URL and added extra data

--- api/hope.js
+++ api/hope.js
@@ -1,25 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const middleware = require('./middleware');
+const express = require("express");
+const cors = require("cors");
+const middleware = require("./middleware");
 const app = express();
 
 app.use(cors());
 
-app.post(
-  '/fetchData/:route/:query?',
-  middleware.understandRequest,
-  (req, res) => {
-    const status = res.isRoute ? 200 : 404;
-    const data = res.data;
-    res
-      .json({
-        isRoute: res.isRoute,
-        ...data
-      })
-      .status(status);
-  }
-);
+app.post("/cms/tovo/v1/:route", middleware.understandRequest, (req, res) => {
+  const status = res.isRoute ? 200 : 404;
+  const data = res.data;
+  res
+    .json({
+      isRoute: res.isRoute,
+      ...data
+    })
+    .status(status);
+});
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
 });
--- api/middleware.js
+++ api/middleware.js
@@ -1,17 +1,22 @@
-const db = require('./db.json');
+const db = require("./db.json");
+const routeJSON = require("./routes.json");
 const understandRequest = (req, res, next) => {
   const requiredRoute = req.params.route;
-  const allRoutes = Object.keys(db);
+  // const allRoutes = Object.keys(db);
 
   // Object Keys
   // filter if query is present
 
-  const result = allRoutes.filter(route => {
-    if (route === requiredRoute) {
-      return true;
-    }
-  });
-  // console.log(result);
+  // const result = allRoutes.filter(route => {
+  //   if (route === requiredRoute) {
+  //     return true;
+  //   }
+  // });
+  const dbRoute = routeJSON[requiredRoute];
+  let result = "";
+  if (dbRoute !== undefined) {
+    result = dbRoute.substring(1);
+  }
   // Add in response
   if (result.length === 0) {
     res.isRoute = false;
@@ -19,28 +24,32 @@ const understandRequest = (req, res, next) => {
   }
 
   // If query is present
-  const query = req.params.query;
-  const dataHolder = db[requiredRoute];
-  // console.log(dataHolder);
+  // const query = req.params.query;
+  // const dataHolder = db[requiredRoute];
+  const dataHolder = db[result];
+  console.log(dataHolder);
 
   res.isRoute = true;
-  if (query === undefined) {
+  if (Object.keys(req.query).length === 0) {
     // All the values of that key
     res.data = dataHolder;
   } else {
     // Check if it array
     if (dataHolder.length) {
+      const query = req.query.customerId;
       // Filter values on the above query
       // Assuming query is id
-      const dataResult = dataHolder.filter(data => {
+      const dataResult = dataHolder.find(data => {
         if (data.id == query) {
-          return true;
+          return data;
         }
       });
       res.data = dataResult;
     }
     // TODO: On specific id return data
-    res.data = dataHolder;
+    else {
+      res.data = dataHolder;
+    }
   }
 
   next();
