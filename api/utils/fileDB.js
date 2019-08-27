ca97ecbf91b194067a60e36134b81fe97ba8ad19
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-08-28 00:45:20 +0530

    Changed Folder name Although it is case insensitive

--- api/utils/fileDB.js
+++ api/utils/fileDB.js
@@ -1,27 +1,27 @@
-const fs = require('fs');
-const path = require('path');
-const HashMap = require('hashmap');
+const fs = require("fs");
+const path = require("path");
+const HashMap = require("hashmap");
 const lowDB = new HashMap();
 
 /**
  * Builds a hashmap to give O(1) lookup
  */
 const buildMap = () => {
-  const DATA_DIR = './JSONDATA';
-  const folders = fs.readdirSync(DATA_DIR);
-  folders.forEach(folder => {
-    const files = fs.readdirSync(path.join(DATA_DIR, folder));
-    files.forEach(file => {
-      lowDB.set(file, path.join(DATA_DIR, folder, file));
+    const DATA_DIR = "./JSONData";
+    const folders = fs.readdirSync(DATA_DIR);
+    folders.forEach(folder => {
+        const files = fs.readdirSync(path.join(DATA_DIR, folder));
+        files.forEach(file => {
+            lowDB.set(file, path.join(DATA_DIR, folder, file));
+        });
     });
-  });
 };
 
 const getMap = () => {
-  return lowDB;
+    return lowDB;
 };
 
 module.exports = {
-  buildMap,
-  getMap
+    buildMap,
+    getMap
 };
