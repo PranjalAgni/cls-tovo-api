ed33c7c0e01b583885ea6c3dc38528b52215979d
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-08-28 00:46:41 +0530

    Added Pagination Though a lot of if-else is necessary Find better solution if possible

--- api/hope.js
+++ api/hope.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const middleware = require('./middleware');
-const morgan = require('morgan');
-const { buildMap } = require('./utils/fileDB');
+const express = require("express");
+const cors = require("cors");
+const middleware = require("./middleware");
+const morgan = require("morgan");
+const { buildMap } = require("./utils/fileDB");
 // For Constants
-const constants = require('./utils/constants');
+const constants = require("./utils/constants");
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 buildMap();
 app.post(
-  `${constants.rrdmsUrlPath}:route`,
-  middleware.understandRequest,
-  (req, res) => {
-    const status = res.isRoute ? 200 : 404;
-    const data = res.data;
-    res
-      .json({
-        isRoute: res.isRoute,
-        ...data
-      })
-      .status(status);
-  }
+    `${constants.rrdmsUrlPath}:route`,
+    middleware.understandRequest,
+    (req, res) => {
+        const status = res.isRoute ? 200 : 404;
+        const data = res.data;
+        res.json({
+            isRoute: res.isRoute,
+            ...data
+        }).status(status);
+    }
 );
 
 const PORT = process.env.PORT || constants.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
--- api/middleware.js
+++ api/middleware.js
@@ -1,65 +1,95 @@
-const stringUtil = require('./utils/stringUtil');
-const { getMap } = require('./utils/fileDB');
+const stringUtil = require("./utils/stringUtil");
+const constants = require("./utils/constants");
+const { getMap } = require("./utils/fileDB");
+
 const lowDB = getMap();
+
 const understandRequest = (req, res, next) => {
-  const pathUptoDo = stringUtil.splitFromStart(req.params.route, '.');
-  const jsonFilePath = pathUptoDo + '.json';
-  // The magic of dynamic db.json starts from here
-  // @PranjalAgni - Hash Map Implementation
-  const dbPath = lowDB.get(jsonFilePath).replace(/\\/g, '/');
-  const jsonDB = require('./../' + dbPath);
+    const pathUptoDo = stringUtil.splitFromStart(req.params.route, ".");
+    const jsonFilePath = pathUptoDo + ".json";
 
-  // Object Keys
-  // filter if query is present
-  let result = '';
-  if (pathUptoDo !== undefined) {
-    result = '/' + pathUptoDo;
-  }
+    // The magic of dynamic db.json starts from here
+    // @PranjalAgni - Hash Map Implementation
+    const dbPath = lowDB.get(jsonFilePath).replace(/\\/g, "/");
+    const jsonDB = require("./../" + dbPath);
 
-  // Add in response
-  if (result.length === 0) {
-    res.isRoute = false;
-    next();
-  }
-
-  // If query is present
-  // const query = req.params.query;
-  // const dataHolder = db[requiredRoute];
-
-  // I(sdc224) have commented this line as it is not required in dynamic JSON
-  // const dataHolder = db[result];
-
-  const dataHolder = jsonDB;
-
-  // console.log(dataHolder);
-
-  res.isRoute = true;
-  console.log(req.query);
-  if (Object.keys(req.query).length === 0) {
-    // All the values of that key
-    res.data = dataHolder;
-  } else {
-    const query = req.query.customerId;
-    // Filter values on the above query
-    // Assuming query is id
-    if (dataHolder.length) {
-      const dataResult = dataHolder.find(data => {
-        if (data.id == query) {
-          return data;
-        }
-      });
-      res.data = dataResult;
-    } else {
-      //TODO: Currently Return all the data
-      res.data = dataHolder;
+    // Object Keys
+    // filter if query is present
+    let result = "";
+    if (pathUptoDo !== undefined) {
+        result = "/" + pathUptoDo;
     }
 
-    //console.log(dataResult);
-  }
+    // Add in response
+    if (result.length === 0) {
+        res.isRoute = false;
+        next();
+    }
 
-  next();
+    // If query is present
+    // const query = req.params.query;
+
+    const dataHolder = jsonDB;
+
+    // console.log(dataHolder);
+
+    res.isRoute = true;
+
+    const requestQuery = req.query;
+
+    const isPageQuery = stringUtil.stringMatch(
+        Object.keys(requestQuery),
+        constants.paginationString
+    );
+
+    if (requestQuery.length === 0) {
+        // All the values of that key
+        res.data = dataHolder;
+    } else {
+        // For pagination (sdc224)
+        if (isPageQuery) {
+            if (dataHolder.workbookItems) {
+                const startingValue =
+                    requestQuery.pageNumber * requestQuery.pageSize;
+                const endingValue =
+                    (requestQuery.pageNumber + 1) * requestQuery.pageSize;
+
+                const slicedData = dataHolder.workbookItems.slice(
+                    startingValue,
+                    endingValue
+                );
+
+                res.data = {
+                    overview: dataHolder.overview,
+                    workbookItems: slicedData
+                };
+            } else {
+                // For upcoming Card
+                res.data = dataHolder;
+            }
+        } else {
+            const query = requestQuery.customerId;
+            // Filter values on the above query
+            // Assuming query is id
+            if (dataHolder.length) {
+                const dataResult = dataHolder.find(data => {
+                    if (data.id == query) {
+                        return data;
+                    }
+                });
+                res.data = dataResult;
+            } else {
+                //TODO: Currently Return all the data
+                res.data = dataHolder;
+            }
+        }
+
+        //console.log(dataResult);
+    }
+
+    next();
 };
 
 module.exports = {
-  understandRequest
+    understandRequest
 };
