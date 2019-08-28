78c49de479316b31b8867f4cfee1473b71aa11a3
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-08-28 11:42:17 +0530

    Added comments in code Please everyone do the comments in ur respective codes

--- api/utils/stringUtil.js
+++ api/utils/stringUtil.js
@@ -1,33 +1,44 @@
 /**
+ * @author Sourodeep Chatterjee
+ * This is the helper JS library which is needed in this API
+ */
+
+/**
+ * @author Sourodeep Chatterjee
  * Splits a given String from starting upto a given Character.
  * @param {String} string The string in which you want to do the operation
  * @param {String} uptoChar The character upto which you want to get the String
  * @returns {String} The modified String...empty if no given character found
+ * @exports splitFromStart
  */
-function splitFromStart(string, uptoChar) {
+splitFromStart = (string, uptoChar) => {
     let index = string.indexOf(uptoChar);
 
     if (index > 0) return string.slice(0, index);
     else return "";
-}
+};
 
 /**
+ * @author Sourodeep Chatterjee
  * Finds difference between two Strings
  * @param {String} bigString The bigger string among both
  * @param {String} smallString The smaller string among both
  * @returns The difference String
+ * @exports stringDifference
  */
-function stringDifference(bigString, smallString) {
-    return bigString.split(smallString).join("");
-}
+stringDifference = (bigString, smallString) => {
+    bigString.split(smallString).join("");
+};
 
 /**
+ * @author Sourodeep Chatterjee
  * Detects whether an array contains the matched String or not... True if partially matched
  * @param {Array} stringArray Array of Strings, in which we need to match the word
  * @param {String} matchWord The string for which we find the match
  * @returns True if matched, False if not matched
+ * @exports stringMatch
  */
-function stringMatch(stringArray, matchWord) {
+stringMatch = (stringArray, matchWord) => {
     for (const string in stringArray) {
         if (stringArray.hasOwnProperty(string)) {
             const element = stringArray[string];
@@ -36,7 +47,7 @@ function stringMatch(stringArray, matchWord) {
     }
 
     return false;
-}
+};
 
 module.exports = {
     splitFromStart,
