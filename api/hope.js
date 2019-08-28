374de4b6f6a52ca7f99017e0dadcc532fed81a9a
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-08-28 16:28:28 +0530

    work progress on fetch communication

--- api/hope.js
+++ api/hope.js
@@ -17,30 +17,36 @@ app.use(morgan("tiny"));
 buildMap();
 //@sdPr1me - route as params for dynamic routing
 app.post(
-  `${constants.rrdmsUrlPath}:route`,
-  middleware.understandRequest,
-  (req, res) => {
-    const status = res.isRoute ? 200 : 404;
-    const data = res.data;
-    res
-      .json({
-        isRoute: res.isRoute,
-        ...data
-      })
-      .status(status);
-  }
+    `${constants.rrdmsUrlPath}:route`,
+    middleware.understandRequest,
+    (req, res) => {
+        const status = res.isRoute ? 200 : 404;
+        const data = res.data;
+        if (typeof data === "Array") {
+            res.json({
+                isRoute: res.isRoute,
+                data
+            }).status(status);
+        }
+        res.json({
+            isRoute: res.isRoute,
+            ...data
+        }).status(status);
+    }
 );
 
 //ashutosh084 - added get functionality for providing images
 app.get(`${constants.imagesUrlPath}/:fileName`, (req, res) => {
-  if (fs.existsSync("./data/images/" + req.params.fileName)) {
-    res.status(202).sendFile(req.params.fileName, { root: "./data/images/" });
-  } else {
-    res.status(404).send("FILE NOT FOUND");
-  }
+    if (fs.existsSync("./data/images/" + req.params.fileName)) {
+        res.status(202).sendFile(req.params.fileName, {
+            root: "./data/images/"
+        });
+    } else {
+        res.status(404).send("FILE NOT FOUND");
+    }
 });
 
 const PORT = process.env.PORT || constants.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
