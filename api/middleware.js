7e4b25dc96709f29c194ddf27a4cd9d6ba56935d
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-08-28 17:33:43 +0530

    Fixed /fetchCommn route :sparkles:

--- api/middleware.js
+++ api/middleware.js
@@ -10,115 +10,113 @@ const { getMap } = require("./Utils/fileDB");
 const lowDB = getMap();
 
 const understandRequest = (req, res, next) => {
-    // Sourodeep Chatterjee
-    // req.params.route is the dynamic route which is coming to us from the cls_tovo_ui
-    // For example, if the page is .../getUserOverview.do, then req.params.route will return getUserOverview.do
+  // Sourodeep Chatterjee
+  // req.params.route is the dynamic route which is coming to us from the cls_tovo_ui
+  // For example, if the page is .../getUserOverview.do, then req.params.route will return getUserOverview.do
 
-    /**
-     * @constant pathUptoDo for splitting the '.do' part from URL
-     */
-    const pathUptoDo = stringUtil.splitFromStart(req.params.route, ".");
+  /**
+   * @constant pathUptoDo for splitting the '.do' part from URL
+   */
+  const pathUptoDo = stringUtil.splitFromStart(req.params.route, ".");
 
-    /**
-     * @constant jsonFilePath for getting the JSON file from that URL
-     */
-    const jsonFilePath = pathUptoDo + ".json";
+  /**
+   * @constant jsonFilePath for getting the JSON file from that URL
+   */
+  const jsonFilePath = pathUptoDo + ".json";
 
-    // @PranjalAgni - Hash Map Implementation
-    const dbPath = lowDB.get(jsonFilePath).replace(/\\/g, "/");
-    const jsonDB = require("./../" + dbPath);
+  // @PranjalAgni - Hash Map Implementation
+  const dbPath = lowDB.get(jsonFilePath).replace(/\\/g, "/");
+  const jsonDB = require("./../" + dbPath);
 
-    // Object Keys
-    // filter if query is present
-    let result = "";
-    if (pathUptoDo !== undefined) {
-        result = "/" + pathUptoDo;
-    }
+  // Object Keys
+  // filter if query is present
+  let result = "";
+  if (pathUptoDo !== undefined) {
+    result = "/" + pathUptoDo;
+  }
 
-    // Add in response
-    if (result.length === 0) {
-        res.isRoute = false;
-        next();
-    }
-
-    const dataHolder = jsonDB;
-
-    res.isRoute = true;
-
-    const requestQuery = req.query;
-
-    /**
-     * @constant isPageQuery a bool denoting whether it is a pagination query
-     */
-    const isPageQuery = stringUtil.stringMatch(
-        Object.keys(requestQuery),
-        constants.paginationString
-    );
-
-    // TODO: Replace it with switch case and move it to other file
-    if (requestQuery.length === 0) {
-        // All the values of that key
-        res.data = dataHolder;
-    } else {
-        // For pagination (sdc224)
-        if (isPageQuery) {
-            if (dataHolder.workbookItems) {
-                const startingValue =
-                    requestQuery.pageNumber * requestQuery.pageSize;
-                const endingValue =
-                    (requestQuery.pageNumber + 1) * requestQuery.pageSize;
-
-                const slicedData = dataHolder.workbookItems.slice(
-                    startingValue,
-                    endingValue
-                );
-
-                res.data = {
-                    overview: dataHolder.overview,
-                    workbookItems: slicedData
-                };
-            } else {
-                // For upcoming Card
-                res.data = dataHolder;
-            }
-        } else {
-            //@sdPr1me - query string present
-            const query = requestQuery.customerId;
-
-            // Filter values on the above query
-            // Assuming query is id
-            if (dataHolder.length) {
-                let dataResult = [];
-
-                if (dataHolder[0].call || dataHolder[0].correspondence) {
-                    const communicationArray = [];
-                    dataHolder.filter(data => {
-                        if (
-                            (data.call && data.call.customerId == query) ||
-                            (data.correspondence &&
-                                data.correspondence.customerId == query)
-                        )
-                            communicationArray.push(data);
-                    });
-
-                    dataResult = communicationArray;
-                } else {
-                    dataResult = dataHolder.find(data => {
-                        if (data.id == query) {
-                            return data;
-                        }
-                    });
-                }
-                res.data = dataResult;
-            } else {
-                res.data = dataHolder;
-            }
-        }
-    }
-
+  // Add in response
+  if (result.length === 0) {
+    res.isRoute = false;
     next();
+  }
+
+  const dataHolder = jsonDB;
+
+  res.isRoute = true;
+
+  const requestQuery = req.query;
+
+  /**
+   * @constant isPageQuery a bool denoting whether it is a pagination query
+   */
+  const isPageQuery = stringUtil.stringMatch(
+    Object.keys(requestQuery),
+    constants.paginationString
+  );
+
+  // TODO: Replace it with switch case and move it to other file
+  if (requestQuery.length === 0) {
+    // All the values of that key
+    res.data = dataHolder;
+  } else {
+    // For pagination (sdc224)
+    if (isPageQuery) {
+      if (dataHolder.workbookItems) {
+        const startingValue = requestQuery.pageNumber * requestQuery.pageSize;
+        const endingValue =
+          (requestQuery.pageNumber + 1) * requestQuery.pageSize;
+
+        const slicedData = dataHolder.workbookItems.slice(
+          startingValue,
+          endingValue
+        );
+
+        res.data = {
+          overview: dataHolder.overview,
+          workbookItems: slicedData
+        };
+      } else {
+        // For upcoming Card
+        res.data = dataHolder;
+      }
+    } else {
+      //@sdPr1me - query string present
+      const query = requestQuery.customerId;
+
+      // Filter values on the above query
+      // Assuming query is id
+      if (dataHolder.length) {
+        let dataResult = [];
+
+        if (dataHolder[0].call || dataHolder[0].correspondence) {
+          const communicationArray = [];
+          dataHolder.filter(data => {
+            if (
+              (data.call && data.call.customerId == query) ||
+              (data.correspondence && data.correspondence.customerId == query)
+            )
+              communicationArray.push(data);
+          });
+
+          dataResult = communicationArray;
+        } else {
+          dataResult = dataHolder.find(data => {
+            if (data.id == query) {
+              return data;
+            }
+          });
+        }
+        res.data = dataResult;
+      } else {
+        res.data = dataHolder;
+      }
+    }
+  }
+
+  next();
 };
 
 module.exports = {
-    understandRequest
+  understandRequest
 };
