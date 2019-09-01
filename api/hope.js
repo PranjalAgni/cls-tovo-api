03d4a397816c45b134eee476dd8bd96985940135
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-09-02 03:12:22 +0530

    some formatting changes in hope.js

--- api/hope.js
+++ api/hope.js
@@ -17,37 +17,35 @@ app.use(morgan("tiny"));
 buildMap();
 //@sdPr1me - route as params for dynamic routing
 app.post(
-  `${constants.rrdmsUrlPath}:route`,
-  middleware.understandRequest,
-  (req, res) => {
-    const status = res.isRoute ? 200 : 404;
-    const result = res.data;
-    const isArray = Array.isArray(result);
-    if (isArray) {
-      res.json(result);
-    } else {
-      res
-        .json({
-          isRoute: res.isRoute,
-          ...result
-        })
-        .status(status);
+    `${constants.rrdmsUrlPath}:route`,
+    middleware.understandRequest,
+    (req, res) => {
+        const status = res.isRoute ? 200 : 404;
+        const result = res.data;
+        const isArray = Array.isArray(result);
+        if (isArray) {
+            res.json(result);
+        } else {
+            res.json({
+                isRoute: res.isRoute,
+                ...result,
+            }).status(status);
+        }
     }
-  }
 );
 
-//@ashutosh084 - added get functionality for providing images
+//ashutosh084 - added get functionality for providing images
 app.get(`${constants.imagesUrlPath}/:fileName`, (req, res) => {
-  if (fs.existsSync("./data/images/" + req.params.fileName)) {
-    res.status(202).sendFile(req.params.fileName, {
-      root: "./data/images/"
-    });
-  } else {
-    res.status(404).send("FILE NOT FOUND");
-  }
+    if (fs.existsSync("./data/images/" + req.params.fileName)) {
+        res.status(202).sendFile(req.params.fileName, {
+            root: "./data/images/",
+        });
+    } else {
+        res.status(404).send("FILE NOT FOUND");
+    }
 });
 
 const PORT = process.env.PORT || constants.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
